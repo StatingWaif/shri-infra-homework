db41cf862db0af04794f32674ed79c97f658b45e
Виктор <StatingWaif68@gmail.com>
2024-07-18 18:47:15 +0300

    dfa

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe("About", () => {
   it("Should render author name", () => {
     const { container } = render(<About />);
 
-    expect(container.textContent).toBe("AboutAuthor: Viktor =)");
+    expect(container.textContent).toBe("AboutAuthor: Viktor d=)");
   });
 });
